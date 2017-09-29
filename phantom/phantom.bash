abc6595792027c8766b9582e9fc0cb99a47af71b
Andy Burton <andy@andyburton.co.uk>
2017-09-29 14:11:09 +0100

    Relative paths in phantom.bash

--- phantom/phantom.bash
+++ phantom/phantom.bash
@@ -1,3 +1,1 @@
-# Replace /PATH-TO-REPO/ with the full path to your code
-
-/PATH-TO-REPO/sfs-developer-toolkit/phantom/phantomjs-macOS /PATH-TO-REPO/sfs-developer-toolkit/phantom/phantom-pdf-generator.js /PATH-TO-REPO/sfs-developer-toolkit/html/sfs-summary-template.html /PATH-TO-REPO/sfs-developer-toolkit/output/sts-summary-test.pdf 210 297 Portrait 1 100
+./phantomjs-macOS ./phantom-pdf-generator.js ../html/sfs-summary-template.html ../output/sts-summary-test-2.pdf 210 297 Portrait 1 100
